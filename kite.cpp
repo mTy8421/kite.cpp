f61077914cc0005d076a73cd89ed0e7b3de73f70
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2022-08-29 23:56:38 +0700

    Update kite.cpp

--- kite.cpp
+++ kite.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
+
+    for (int i = 1; i <= 4 ; i++)
+    {
+        for (int j = 1; j <= 2 * (3 - i + 1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * (i * 3 - (i + 1)) -1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    
+    
     return EXIT_SUCCESS;
 }
