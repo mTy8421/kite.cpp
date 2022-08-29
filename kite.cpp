b7b1cc273930ad3cdeaabd00e2cd33c04ff6ca4c
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2022-08-30 00:05:13 +0700

    update

--- kite.cpp
+++ kite.cpp
@@ -17,7 +17,6 @@ int main()
         cout << endl;
     }
     
-
     for (int i = 3; i <= 6; i++)
     {
         for (int j = 1; j <= i; j++)
@@ -31,13 +30,14 @@ int main()
         cout << endl;
     }
     
+    
     for (int i = 1; i <= 6 ; i++)
     {
         for (int j = 1; j <= i; j++)
         {
             cout << " ";
         }
-        for (int j = 1; j <= 2 * (6 - i + 1); j++)
+        for (int j = 1; j <= 2 * (6 - i + 1) -1; j++)
         {
             cout << "*";
         }
